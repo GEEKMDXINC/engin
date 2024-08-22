c52a5b3d09c03ebf136f471ca3e293dfed5de0b9
GEEKMDXINC <herukobot67@gmail.com>
2024-08-22 06:47:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority",
   SESSION_NAME: process.env.SESSION_NAME || "Bot~"
 };
